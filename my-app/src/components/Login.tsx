0faa2ff60dc04f65347a9b3d17da8942b401225e
Flurin Brühwiler <47397416+FlurinBruehwiler@users.noreply.github.com>
2022-03-19 14:20:02 +0100

    feat(Login): Added Login [19.03.2022]

--- my-app/src/components/Login.tsx
+++ my-app/src/components/Login.tsx
@@ -1,15 +1,67 @@
 import Button from "./Button";
 import Textbox from "./Textbox";
-import React, { useState } from "react";
+import { AiFillPlusCircle } from "react-icons/ai";
+import React, { useEffect, useState } from "react";
+import axios from "../axios";
 
 export default () => {
-  return (
-    <div className="min-w-[25rem] flex flex-col items center justify-center">
-      <div className=" flex flex-row w-80">
-        <div className="text-text"></div>
-      </div>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSending, setIsSenting] = useState(false);
 
-      <div className="items center space-x-5"></div>
+  const changeUsername = (childData: string) => setUsername(childData);
+  const changePassword = (childData: string) => setPassword(childData);
+
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const submit = () => {
+    if (isSending) return;
+
+    setUsernameError("");
+    setPasswordError("");
+
+    async function sendData() {
+      setIsSenting(true);
+      const request = await axios
+        .post("/Auth/login", {
+          username: username,
+          password: password,
+        })
+        .catch((error) => {
+          if (error.response.data.includes("User")) {
+            setUsernameError(error.response.data);
+          } else {
+            setPasswordError(error.response.data);
+          }
+        });
+      setIsSenting(false);
+    }
+    sendData();
+  };
+
+  return (
+    <div className="min-w-[25rem] flex flex-col items-center">
+      <div className="grid grid-cols-2 gap-x-5 gap-y-5 mt-52">
+        <Textbox
+          label={"Username"}
+          parentCallback={changeUsername}
+          errorText={usernameError}
+        />
+        <Textbox
+          label={"Password"}
+          parentCallback={changePassword}
+          type={"password"}
+          errorText={passwordError}
+        />
+      </div>
+      <div className="flex justify-end">
+        <Button
+          text={"Create Account"}
+          clickCallback={submit}
+          isLoading={isSending}
+        />
+      </div>
     </div>
   );
 };
